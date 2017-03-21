b2872db204a714048a0a29f6cb3c7d3fb05a9ede
itzthanh <thanhhd@uci.edu>
2017-03-20 22:59:13 -0700

    Add files via upload

--- js/form_data.js
+++ js/form_data.js
@@ -1,20 +1,43 @@
 $(document).ready(function(){
-	//$(".checkboxsection").hide();
-	var $currentGoalCounter = 1;
+	var goalScoreArray = {};
+	var $currentGoalCounter = 0;
 	$(".goals").each(function(){
+		addScoreInputField($currentGoalCounter, this);
 		addCheckbox($currentGoalCounter, this);
 		$currentGoalCounter++;
-		//var $checkboxClone = $(this).find($clone);
-			//$(this)
-			//.attr("id", $IDcounter+1);
 	});
+	for (var i=0; i<$currentGoalCounter; i++){
+		goalScoreArray[i] = 0;
+	}
+
+	$(".checkboxstyle").click(function(){
+		$(this).toggleClass("clicked");
+		var $score = goalScoreArray[$(this).parents(".goals").attr("id").split("s")[1]];
+		if ($(this).hasClass("clicked")){
+			goalScoreArray[$(this).parents(".goals").attr("id").split("s")[1]] = ++$score;
+			
+		} 
+		else if (!($(this).hasClass("clicked")) && $score > 0){
+			goalScoreArray[$(this).parents(".goals").attr("id").split("s")[1]] = --$score;
+		}
+		for (var i=0; i<$currentGoalCounter; i++){
+			console.log($("#goalScore" + (i + 1)));
+			$("#goalScore" + (i + 1)).val(goalScoreArray[i].toString());
+		}
+	});
+
 });
+
+function addScoreInputField(goalNum, appendToContainer){
+	$(appendToContainer).append("<p>Passes (out of 10): </p>");
+	++goalNum;
+	$("<input>", {"class": "goalScore", "id":"goalScore" + goalNum,"type":"number", "name":"GoalScores[]" + goalNum + "Score", "min":"0", "max":"10", "value":""}).appendTo(appendToContainer);
+}
 
 function addCheckbox(goalNum, appendToContainer) {
 	$("<p class=\"trial\">Trial:</p>").css({"margin-bottom":"0"}).appendTo(appendToContainer);
 	var container = $("<div>", {"class": "checkboxsection"}).appendTo(appendToContainer);
-	var displayGoalNum = 1;
-	if (goalNum == 1){
+	if (goalNum == 0){
 		for (var i = 1; i <= 10; i++){
 			var checkbox = $("<div>", {"class": "checkbox w-checkbox w-clearfix"}).appendTo(container);
 			var checkboxInput = checkbox.append($("<input>", {"class": "display-none w-checkbox-input", "data-name":"Checkbox"+i, 
@@ -26,39 +49,13 @@ function addCheckbox(goalNum, appendToContainer) {
 	else{
 		var id = $(appendToContainer).prev().find("input").last().attr("id").split("-")[1];
 		for (var i = 1; i <= 10; i++){
+			++id;
 			var checkbox = $("<div>", {"class": "checkbox w-checkbox w-clearfix"}).appendTo(container);
 			var checkboxInput = checkbox.append($("<input>", {"class": "display-none w-checkbox-input", "data-name":"Checkbox"+id, 
 			id: "checkbox-" + id, name:"checkbox-" + id, type: "checkbox"}));
 			var checkboxLabel = checkbox.append($("<label>", {"class": "checkboxstyle w-form-label", "data-ix": "checkboxinteraction", 
 			for: "checkbox-" + id, text:i}));
-			id++;
+
 		}
 	}
 }
-			/*var checkbox = $("<div>", {"class": "checkbox w-checkbox w-clearfix"}).appendTo(container);
-			var checkboxInput = checkbox.append($("<input>", {"class": "display-none w-checkbox-input", "data-name":"Checkbox"+id, 
-			id: "checkbox-" + id, name:"checkbox-" + id, type: "checkbox"}));
-			var checkboxLabel = checkbox.append($("<label>", {"class": "checkboxstyle w-form-label", "data-ix": "checkboxinteraction", 
-			for: "checkbox-" + id, text:displayGoalNum}));
-		}
-		//console.log($(appendToContainer).prev().find("input").last().attr("id").split("-")[1]);
-		//for (var i = appendTo.find(".checkbox").last().
-	}
-}
-
-
-
-
-   	/*var container = $('#cblist');
-   	var inputs = container.find('input');
-   	var id = inputs.length+1;
-
-   $('<input />', { type: 'checkbox', id: 'cb'+id, value: name }).appendTo(container);
-   $('<label />', { 'for': 'cb'+id, text: name }).appendTo(container);
-}
-
-<div class="checkboxsection">
-          <div class="checkbox w-checkbox w-clearfix">
-            <input class="display-none w-checkbox-input" data-name="Checkbox" id="checkbox-00" name="checkbox-00" type="checkbox">
-            <label class="checkboxstyle w-form-label" data-ix="checkboxinteraction" for="checkbox-00">1</label>
-          </div>*/
